56702ab209d5db299ad8ee715f60296d29b001d5
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-08-18 00:31:07 +0900

    fix user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     sequence(:name) { |n| "test#{n}" }
     sequence(:email) { |n| "test#{n}@test.com" }
     password "123456"
-    password_confirmation password
+    password_confirmation "123456"
   end
 end
