88889c137357e6caacb797f8bfa2017963a5a230
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-09-01 14:49:33 +0900

    Add specs for the PostsForBranchService

--- app/services/posts_for_branch_service.rb
+++ app/services/posts_for_branch_service.rb
@@ -1,5 +1,5 @@
 class PostsForBranchService
-  
+
   def initialize(params)
     @search = params[:search]
     @category = params[:category]
@@ -8,14 +8,14 @@ class PostsForBranchService
 
   # get posts depending on the request
   def call
-    if category.blank? && search.blank?
-      posts = Post.by_branch(branch).all
-    elsif category.blank? && search.present?
-      posts = Post.by_branch(branch).search(search)
-    elsif category.present? && search.blank?
-      posts = Post.by_category(branch, category)
-    elsif category.present? && search.present?
-      posts = Post.by_branch(branch, category).search(search)
+    if @category.blank? && @search.blank?
+      posts = Post.by_branch(@branch).all
+    elsif @category.blank? && @search.present?
+      posts = Post.by_branch(@branch).search(@search)
+    elsif @category.present? && @search.blank?
+      posts = Post.by_category(@branch, @category)
+    elsif @category.present? && @search.present?
+      posts = Post.by_category(@branch, @category).search(@search)
     else
     end
   end
